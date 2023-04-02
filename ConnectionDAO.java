fc767d6e3d9112f7b1ea0bff60e04d3a18a0b7fd
arnold onana <117309561+arnold-11-lepeace@users.noreply.github.com>
2023-04-03 00:18:54 +0200

    Update ConnectionDAO.java

--- ConnectionDAO.java
+++ ConnectionDAO.java
@@ -1,26 +1,17 @@
 package DAO;
 
 /**
- * Classe d'acces a la base de donnees
+ * Classe de connexion à la base de données
  * 
- * @author ESIGELEC - TIC Department
- * @version 2.0
+ * @author Arnold  ONANA
+ * @version 1.0
  * */
 
 public class ConnectionDAO {
-	
-		/**
-		 * Parametres de connexion a la base de donnees oracle
-		 * URL, LOGIN et PASS sont des constantes
-		 */
-		// � utiliser si vous �tes sur une machine personnelle :
 		final static String URL   = "jdbc:oracle:thin:@oracle.esigelec.fr:1521:orcl";
-		
-		// � utiliser si vous �tes sur une machine de l'�cole :
-		//final static String URL   = "jdbc:oracle:thin:@//srvoracledb.intranet.int:1521/orcl.intranet.int";
 
-		final static String LOGIN = "C##BDD4_26";   // remplacer les ********. Exemple C##BDD1_1 C##BDD4_26
-		final static String PASS  = "BDD426";   // remplacer les ********. Exemple BDD11        BDD426
+		final static String LOGIN = "LOGIN";   
+		final static String PASS  = "MOT DE PASSE";   
 		
 		/**
 		 * Constructor
