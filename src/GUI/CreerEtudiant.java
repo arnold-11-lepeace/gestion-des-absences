162443c0284440fa47ce52f003f9b30de11d36e4
Arnold ONANA ANDELA <onana.andel@gmail.com>
2023-04-25 01:34:43 +0200

    insertion de placeholder dans la classe creer un étudiant

--- src/GUI/CreerEtudiant.java
+++ src/GUI/CreerEtudiant.java
@@ -15,6 +15,9 @@ import Model.Eleve;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import javax.swing.SwingConstants;
 
 public class CreerEtudiant {
 
@@ -23,6 +26,8 @@ public class CreerEtudiant {
 	private JTextField txtPrnom;
 	private JTextField txtAdressemail;
 	private JTextField txtFilire;
+	private JTextField txtAjoutReussi;
+	private JTextField txtMessageErreur;
 
 	/**
 	 * Launch the application.
@@ -81,32 +86,112 @@ public class CreerEtudiant {
 		panel_1.setLayout(null);
 		
 		txtNom = new JTextField();
+		txtNom.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				if(txtNom.getText().equals("Nom")) {
+					txtNom.setText("");
+				}
+				
+			}
+			@Override
+			public void focusLost(FocusEvent e) {
+				if(txtNom.getText().equals("")) {
+					txtNom.setText("Nom");
+				}
+			}
+		});
 		txtNom.setFont(new Font("Tahoma", Font.ITALIC, 14));
 		txtNom.setText("Nom");
-		txtNom.setBounds(132, 28, 201, 20);
+		txtNom.setBounds(132, 42, 201, 20);
 		panel_1.add(txtNom);
 		txtNom.setColumns(20);
 		
 		txtPrnom = new JTextField();
+		txtPrnom.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				if(txtPrnom.getText().equals("Prénom")) {
+					txtPrnom.setText("");
+				}
+				
+			}
+			@Override
+			public void focusLost(FocusEvent e) {
+				if(txtPrnom.getText().equals("")) {
+					txtPrnom.setText("Prénom");
+				}
+			}
+		});
 		txtPrnom.setText("Prénom");
 		txtPrnom.setFont(new Font("Tahoma", Font.ITALIC, 14));
 		txtPrnom.setColumns(20);
-		txtPrnom.setBounds(132, 59, 201, 20);
+		txtPrnom.setBounds(132, 73, 201, 20);
 		panel_1.add(txtPrnom);
 		
 		txtAdressemail = new JTextField();
+		txtAdressemail.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				if(txtAdressemail.getText().equals("Adressemail")) {
+					txtAdressemail.setText("");
+				}
+				
+			}
+			@Override
+			public void focusLost(FocusEvent e) {
+				if(txtAdressemail.getText().equals("")) {
+					txtAdressemail.setText("Adressemail");
+				}
+			}
+		});
 		txtAdressemail.setText("Adressemail");
 		txtAdressemail.setFont(new Font("Tahoma", Font.ITALIC, 14));
 		txtAdressemail.setColumns(20);
-		txtAdressemail.setBounds(132, 90, 201, 20);
+		txtAdressemail.setBounds(132, 104, 201, 20);
 		panel_1.add(txtAdressemail);
 		
 		txtFilire = new JTextField();
+		txtFilire.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				if(txtFilire.getText().equals("Filière")) {
+					txtFilire.setText("");
+				}
+				
+			}
+			@Override
+			public void focusLost(FocusEvent e) {
+				if(txtFilire.getText().equals("")) {
+					txtFilire.setText("Filière");
+				}
+			}
+		});
 		txtFilire.setText("Filière");
 		txtFilire.setFont(new Font("Tahoma", Font.ITALIC, 14));
 		txtFilire.setColumns(20);
-		txtFilire.setBounds(132, 121, 201, 20);
+		txtFilire.setBounds(132, 135, 201, 20);
 		panel_1.add(txtFilire);
+		
+		txtMessageErreur = new JTextField();
+		txtMessageErreur.setForeground(new Color(255, 0, 0));
+		txtMessageErreur.setEditable(false);
+		txtMessageErreur.setText("Ajout impossible. Veuillez remplir tous les champs");
+		txtMessageErreur.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		txtMessageErreur.setColumns(20);
+		txtMessageErreur.setBounds(72, 11, 315, 20);
+		txtMessageErreur.setVisible(false);
+		panel_1.add(txtMessageErreur);
+		
+		txtAjoutReussi = new JTextField();
+		txtAjoutReussi.setEditable(false);
+		txtAjoutReussi.setForeground(new Color(0, 255, 64));
+		txtAjoutReussi.setText("Ajout réussi");
+		txtAjoutReussi.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		txtAjoutReussi.setColumns(20);
+		txtAjoutReussi.setBounds(132, 11, 201, 20);
+		txtAjoutReussi.setVisible(false);
+		panel_1.add(txtAjoutReussi);
 		
 		
 		EleveDao EleveDAO = new EleveDao();
@@ -119,13 +204,23 @@ public class CreerEtudiant {
 				Eleve E1 = new Eleve( 10013345,"azertyu",txtNom.getText(),txtPrnom.getText(), 
 						txtAdressemail.getText(),txtFilire.getText());
 				EleveDAO.add(E1);
+				if(txtNom.getText().equals("Nom")||txtPrnom.getText().equals("Prénom")|| txtAdressemail.getText().equals("Adressemail") || txtFilire.getText().equals("Filière"))
+				{
+					txtMessageErreur.setVisible(true);
+					txtAjoutReussi.setVisible(false);
+				}
+				else {
+					txtMessageErreur.setVisible(false);
+					txtAjoutReussi.setVisible(true
+							);
+				}
 				
 				
 				
 			}
 		});
 		btnNewButton_1.setBackground(new Color(128, 255, 128));
-		btnNewButton_1.setBounds(315, 152, 89, 23);
+		btnNewButton_1.setBounds(315, 166, 89, 23);
 		panel_1.add(btnNewButton_1);
 		
 		JButton btnNewButton_2 = new JButton("Retour");
@@ -133,11 +228,16 @@ public class CreerEtudiant {
 			public void actionPerformed(ActionEvent e) {
 				Acceuil window = new Acceuil();
 				CreerEtudiant.setVisible(false);
+				
 			}
 		});
 		btnNewButton_2.setBackground(new Color(128, 128, 255));
-		btnNewButton_2.setBounds(104, 152, 89, 23);
+		btnNewButton_2.setBounds(104, 166, 89, 23);
 		panel_1.add(btnNewButton_2);
+		
+		
+		
+		
 		
 		
 	}
